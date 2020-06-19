e9befe14b9bc50b7e9721691742a9ed90b31b25e
Noxseras <tzouliano10@gmail.com>
2020-06-20 02:39:15 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 print('Hello World!')
-
 print('This is a sample application')
+ask_user = str(input('Hello there! Was everything okey today? :'))
+if ask_user == 'yes':
+    print('Happy to hear that you are okey!!')
+elif ask_user =='bad':
+    print('Do not feel bad, everything will be okey!!')
+else:
+    print('I hope that everything\'s okey!')
